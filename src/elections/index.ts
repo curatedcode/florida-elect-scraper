84dad4fead67ae8a7fbf0de5b2f11dc4391d96b1
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2025-05-08 05:43:00 -0400

    renames process config

--- src/elections/index.ts
+++ src/elections/index.ts
@@ -4,15 +4,12 @@ import { type ProcessFilesArgs, processFiles } from "./processFiles.js";
 
 export type RunArgs = {
 	crawlerOptions?: CrawlerArgs;
-	processFilesOptions?: ProcessFilesArgs;
+	processOptions?: ProcessFilesArgs;
 };
 
-export async function run({
-	crawlerOptions,
-	processFilesOptions,
-}: RunArgs = {}) {
+export async function run({ crawlerOptions, processOptions }: RunArgs = {}) {
 	await crawler(crawlerOptions);
-	await processFiles(processFilesOptions);
+	await processFiles(processOptions);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
