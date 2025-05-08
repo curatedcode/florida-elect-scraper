0a1a66db301501e37cc2b0e4683dcfaac6e5abf5
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2025-05-08 03:30:47 -0400

    remove senator formatting function

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,8 @@ import { run as contributionsCrawler } from "./elections/index.js";
 import { run as electionCrawler } from "./elections/index.js";
 import { run as senatorCrawler } from "./senators/index.js";
 
-// biome-ignore lint/correctness/noUnusedImports: Leave in for fast switching
-import { formatForContributionsScrape } from "./senators/formatForContributionsScrape.js";
-
 (async () => {
-	/**
-	 * DEFAULT: contributionCrawler will use the NAMES_TO_SCRAPE.json file
-	 */
-	await Promise.all([senatorCrawler, contributionsCrawler, electionCrawler]);
-
-	/**
-	 * Crawler will use the data from senatorCrawler
-	 */
-	// await Promise.all([senatorCrawler, contributionsCrawler(await formatForContributionsScrape()), electionCrawler])
+	await senatorCrawler();
+	await contributionsCrawler();
+	await electionCrawler();
 })();
